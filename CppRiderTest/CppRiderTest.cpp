862cac1df6a69a7b3278911f058ecb2e64ce72d3
SnoodieTheSnoople <cleefgulfan312@gmail.com>
2022-11-15 20:00:07 +0000

    - Added new functions (UNFINISHED) functions to execute operations and to find a result.
    - Finished toVector() function to allow the calculator to read any expression and add the values and operators into a vector. This makes it easier to read and removes the issue of whitespace or any inconsistencies when reading an expression.
    
    - Few TODO notes so the unfinished functions have a possible solution that can be added to later on.

--- CppRiderTest/CppRiderTest.cpp
+++ CppRiderTest/CppRiderTest.cpp
@@ -1,11 +1,19 @@
 
 #include <iostream>
+#include <regex>
 #include <string>
+#include <valarray>
 #include <vector>
 
 #include "Maths.h"
 
-static void basicMaths()
+void menu()
+{
+    std::cout << "Welcome to the console C++ Calculator app." << std::endl <<
+        "Please enter your mathematical problem below." << std::endl << std::endl;
+}
+
+void basicMaths()
 {
     Maths maths;
     int count = 0;
@@ -23,7 +31,6 @@ static void basicMaths()
             std::cout << std::endl;
             sum = maths.add(sum, sNum);
             std::cout << sum << std::endl; //Prints results
-            //TODO: Create a stack inside the loop and constantly add until there is a break
         }
         else
         {
@@ -35,20 +42,94 @@ static void basicMaths()
     }
 }
 
-static void toVector(std::string s)
+long doOperation(char op, long firstDig, long secDig)
 {
+    Maths maths;
+    char operators[] = {'+', '-', '*', '/'};
+    std::cout << *(&operators + 1) - operators << std::endl;
+    long resultOfOperation = 0.0;
+    
+    for (int x = 0; x < *(&operators + 1) - operators ; x++)
+    {
+        switch (operators[x])
+        {
+        case '+':
+            resultOfOperation += maths.add(firstDig, secDig);
+            break;
+
+        case '-':
+            resultOfOperation += maths.subtract(firstDig, secDig);
+            break;
+
+        case '*':
+            resultOfOperation += maths.multiply(firstDig, secDig);
+            break;
+
+        case '/':
+            resultOfOperation += maths.divide(firstDig, secDig);
+            break;
+            
+        default: ;
+        }
+    }
+
+    return resultOfOperation;
+}
+
+long findResult(std::vector<std::string> expression)
+{;
+    char op;
+    long firstDig = NULL;
+    long secDig = NULL;
+    long result = 0.0;
+
+    for (int x = 0; x < expression.size(); x++)
+    {
+        if (firstDig != NULL && secDig != NULL)
+        {
+            result = doOperation(op, firstDig, secDig);
+            firstDig = NULL;
+            secDig = NULL;
+        }
+        //TODO: Make sure it checks if the value indexed is an operator. Can possibly use std::isDigit()
+        if (x == 0 || x % 2 == 1)
+        {
+            firstDig = std::stol(expression[x]);
+        }
+        else if (x % 2 == 0)
+        {
+            secDig = std::stol(expression[x]);
+        }
+        
+        //If list is even, fill firstDig and secDig and add to the result.
+        //If list is odd, add firstDig at the end to the result
+    }
+
+    if (secDig == NULL) { result = doOperation(op, firstDig, secDig); }
+
+    return result; 
+}
+
+void toVector(std::string s)
+{
+    char operators[] = {'+', '-', '*', '/'}; //Used inside for loop to add operator inside the std::vector
     std::vector<std::string> sList;
     std::string value = "";
     std::cout << "Size of string: " << s.size() << std::endl;
     for (int x = 0; x < s.size(); x++)
     {
-        if (s[x] != ' ')
+        if (std::isdigit(s[x]))
         {
+            //If s[x] is a digit it will add to the std::string value.
             value += s[x];
-            //std::cout << value << std::endl;
+            std::cout << "Value = " << value << std::endl;
         }
-        else
+        else if (std::find(std::begin(operators), std::end(operators), s[x]) != std::end(operators))
         {
+            //std::cout << s[x] << std::endl;
+            sList.push_back(value);
+            value = "";
+            value += s[x];
             sList.push_back(value);
             value = "";
         }
@@ -57,21 +138,19 @@ static void toVector(std::string s)
 
     /*
     Testing:
-    */
+    - Outputs the values inside the vector
     std::cout << "Size of the vector: " << sList.size() << std::endl;
     for (int y = 0; y < sList.size(); y++)
     {
         std::cout << sList.at(y) << std::endl;
     }
-    
+    */
 }
 
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Welcome to the console C++ Calculator app." << std::endl <<
-        "Please enter your mathematical problem below." << std::endl << std::endl;
-    //TODO: Create menu method to display different operations
+    menu();
     //Maths maths;
     int count = 0;
     std::string uInput = "";
